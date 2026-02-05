f18d5b0fcaabb3e676c17f1af5d24bbb4d5519c5
villegas07 <brayanvillegas0719@gmail.com>
2026-02-04 21:55:12 -0500

    roles y permisos por roles

--- src/main.ts
+++ src/main.ts
@@ -65,6 +65,6 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`✓ Application running on: http://localhost:${port}`);
-  console.log(`✓ Swagger documentation: http://localhost:${port}/api/docs`);
+  console.log(`✓ Swagger documentation: http://localhost:${port}/docs`);
 }
 bootstrap();
